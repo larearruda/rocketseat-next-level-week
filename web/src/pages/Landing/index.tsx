34e0c027d225494ef72ee2fd7ff5c4672209bf8a
Larissa Arruda <larissa.arruda94@gmail.com>
2020-08-10 11:17:19 -0300

    desenhando layout da Landing Page

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
+// import '../../assets/styles/global.css';
+
+function Landing(){
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+
+                <div className="logo-container">
+                    <img src={logoImg} alt="Proffy Logo" />
+                    <h2>Sua plataforma de estudos online</h2>
+                </div>
+
+                <img src={landingImg} alt="Landing" className="hero-image"/>
+
+                <div className="buttons-container">
+                    <a href="/" className="study">
+                        <img src={studyIcon} alt="Estudar Icon" />
+                        Estudar
+                    </a>
+
+                   <a href="/" className="give-classes">
+                        <img src={giveClassIcon} alt="Dar Aulas Icon" />
+                        Dar Aulas
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Total de 200 conexões já realizadas   <img src={purpleHeartIcon} alt="Coração Roxo" />
+                </span>
+            </div>
+        </div>
+    );
+
+}
+
+export default Landing;
